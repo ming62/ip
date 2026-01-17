15feeaf76ce2f76bb91a56528136d16dc20bb399
MingYou <limmingyou@gmail.com>
2026-01-17 15:38:38 +0800

    Implement Level 1: Echo
    Modify the display format and the improve Gus
    such that it echoes inputs from user
    
    Gus will exit when user input 'bye'

--- src/main/java/Gus.java
+++ src/main/java/Gus.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class Gus {
     public static String gusPrefix = "[ GUSTAVO ]: ";
     public static String line = "______________________________________________";
+    public static String endString = "bye";
 
     public static void main(String[] args) {
 
@@ -11,11 +13,22 @@ public class Gus {
                     + "         ██║   ██║██║   ██║╚════██║\n"
                     + "         ╚██████╔╝╚██████╔╝███████║\n"
                     + "          ╚═════╝  ╚═════╝ ╚══════╝\n";
+        System.out.println("\nHello my name is Gustavo but you can call me" );
         System.out.println(line);
-        System.out.println("Hello my name is Gustavo but you can call me\n\n" + logo);
+        System.out.println("\n\n" + logo);
         System.out.println(line);
-        System.out.println(gusPrefix + "How can i help you today");
+        System.out.println("\n" + gusPrefix + "How can i help you today");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals(endString)) {
+            System.out.println(gusPrefix + input);
+            input = scanner.nextLine();
+        }
+
         System.out.println(gusPrefix + "Thats all for today");
+        scanner.close();
 
     }
 }
