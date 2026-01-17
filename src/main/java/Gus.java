1218d7f13e9f9ab3f37916cef29d9402038d0cdf
MingYou <limmingyou@gmail.com>
2026-01-17 15:14:32 +0800

    Implement Level 0: Rename, Greet, Exit
    Rename the project from Duke to Gus and complete skeletal.

---
+++ src/main/java/Gus.java
@@ -0,0 +1,21 @@
+
+public class Gus {
+    public static String gusPrefix = "[ GUSTAVO ]: ";
+    public static String line = "______________________________________________";
+
+    public static void main(String[] args) {
+
+        String logo = "          ██████╗ ██╗   ██╗███████╗\n"
+                    + "         ██╔════╝ ██║   ██║██╔════╝\n"
+                    + "         ██║  ███╗██║   ██║███████╗\n"
+                    + "         ██║   ██║██║   ██║╚════██║\n"
+                    + "         ╚██████╔╝╚██████╔╝███████║\n"
+                    + "          ╚═════╝  ╚═════╝ ╚══════╝\n";
+        System.out.println(line);
+        System.out.println("Hello my name is Gustavo but you can call me\n\n" + logo);
+        System.out.println(line);
+        System.out.println(gusPrefix + "How can i help you today");
+        System.out.println(gusPrefix + "Thats all for today");
+
+    }
+}
